be9cdbbec515c3d6f0d24552e04019c9cec1b02a
Hadoukenlol <noobs.ololo@gmail.com>
2024-08-19 12:30:24 +0300

    Добавьте файлы проекта.

---
+++ EntityFramework/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Database=testing;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
